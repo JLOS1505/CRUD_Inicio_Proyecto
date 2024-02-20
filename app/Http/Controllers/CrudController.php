35746d4c4d489d16f4651c44cd91638abcd9630a
unknown <jlos150502@gmail.com>
2024-02-19 20:33:11 -0600

    Controlador de tabla producto, con funcion Create

---
+++ app/Http/Controllers/CrudController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CrudController extends Controller
+{
+    public function index()
+    {
+        $datos = DB::select("select * from producto");
+        return view("welcome")->with("datos", $datos);
+    }
+
+    public function create(Request $request)
+    {
+
+        try {
+            $sql = DB::insert("insert into producto(id_producto, nombre, precio, cantidad)values(?, ?, ?, ?) ", [
+                $request->txtcodigo,
+                $request->txtnombre,
+                $request->txtprecio,
+                $request->txtcantidad
+            ]);
+        } catch (\Throwable $th) {
+            $sql = 0;
+        }
+
+        if ($sql == true) {
+            return back()->with("correcto", "Producto registrado correctamente");
+        } else {
+            return back()->with("incorrecto", "Error el registrar");
+        }
+    }
+}
